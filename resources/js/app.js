587a79416d60e42a2507f098f86e140492f227fa
Ehsan Farooqi <e_a_farooqi@hotmail.com>
2024-02-23 22:47:29 +0100

    Bootstrap 5 added Tailwind CSS removed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,14 @@
 import './bootstrap';
 
+// Bootstrap 5
+import * as bootstrap from 'bootstrap';
+window.bootstrap = bootstrap;
+
+// Bootstrap Toaster Plugin
+import { Toast, TOAST_STATUS, TOAST_THEME, TOAST_PLACEMENT } from "bootstrap-toaster";
+window.Toast = Toast;
+
+// Alpine JS
 import Alpine from 'alpinejs';
-
 window.Alpine = Alpine;
-
 Alpine.start();
