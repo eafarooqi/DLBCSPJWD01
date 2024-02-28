f5baaff806b74b71e8e1c0b055e7430c52b548a0
DEEHFAR1 <ehsanahmed.farooqi@de.toyota-industries.eu>
2024-02-28 10:57:58 +0100

    wip: profile template

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,47 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
+@section('breadcrumbs')
+
+@endsection
+
+@section('content')
+
+    <main class="content">
+        <div class="container-fluid p-0">
+
+            <div class="mb-3">
+                <h1 class="h3 d-inline align-middle">{{ __('Profile') }}</h1>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
-            </div>
+            <div class="row">
+                <div class="col-12 mb-4">
+                    <x-template.notification />
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
+                    <div class="py-12">
+                        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+                            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                                <div class="max-w-xl">
+                                    @include('profile.partials.update-profile-information-form')
+                                </div>
+                            </div>
+
+                            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                                <div class="max-w-xl">
+                                    @include('profile.partials.update-password-form')
+                                </div>
+                            </div>
+
+                            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                                <div class="max-w-xl">
+                                    @include('profile.partials.delete-user-form')
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
-    </div>
-</x-app-layout>
+    </main>
+
+@endsection
