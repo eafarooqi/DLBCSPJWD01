5680f95f4944610a4a0bc65d283dcf09b4320fbb
Ehsan Farooqi <e_a_farooqi@hotmail.com>
2024-03-08 22:14:18 +0100

    sub category db structure changed to one table

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,17 +2,15 @@
 
 namespace App\Models;
 
-use App\Models\Scopes\UserScope;
-use Illuminate\Database\Eloquent\Attributes\ScopedBy;
+use App\Traits\HasUserTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-#[ScopedBy([UserScope::class])]
 class Category extends Model
 {
-    use SoftDeletes, HasFactory;
+    use SoftDeletes, HasFactory, HasUserTrait;
 
     public $timestamps = false;
 
@@ -20,8 +18,23 @@ class Category extends Model
         'name',
     ];
 
+    /**
+     * Get the user to whom this category belongs to
+     *
+     * @return BelongsTo
+     */
     protected function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the parent category
+     *
+     * @return BelongsTo
+     */
+    protected function parent(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
     }
 }
--- database/migrations/2024_02_29_144549_categories.php
+++ database/migrations/2024_02_29_144549_categories.php
@@ -11,30 +11,19 @@ return new class extends Migration {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('parent_id')->default(0);
             $table->string('name');
 
             $table->softDeletes();
 
             // Foreign Key
             $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete()->cascadeOnUpdate();
-        });
-
-        // Sub Categories table
-        Schema::create('sub_categories', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('parent_id');
-            $table->string('name');
-
-            $table->softDeletes();
-
-            // Foreign Key
             $table->foreign('parent_id')->references('id')->on('categories')->cascadeOnDelete()->cascadeOnUpdate();
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('sub_categories');
         Schema::dropIfExists('categories');
     }
 };
