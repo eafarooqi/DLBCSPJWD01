501f814854d28a228a10e39a671cebf4b8240fef
Ehsan Farooqi <e_a_farooqi@hotmail.com>
2024-03-29 21:18:43 +0100

    genreRequest class renamed

---
+++ app/Http/Requests/GenreRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class GenreRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'max:191'],
+        ];
+    }
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
