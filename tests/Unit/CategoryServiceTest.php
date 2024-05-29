41a570bab17a4561b634f941cc616527a304fe48
DEEHFAR1 <ehsanahmed.farooqi@de.toyota-industries.eu>
2024-05-29 15:08:36 +0200

    wip: add category function test

--- tests/Unit/CategoryServiceTest.php
+++ tests/Unit/CategoryServiceTest.php
@@ -6,6 +6,7 @@ use App\Models\Category;
 use App\Models\User;
 use App\Services\CategoryService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 
 class CategoryServiceTest extends TestCase
@@ -20,5 +21,34 @@ class CategoryServiceTest extends TestCase
 
         // Initialize the CategoryService
         $this->categoryService = new CategoryService;
+    }
+
+    public function test_add_category()
+    {
+        $user = User::factory()->make();
+        // Sample data for the new category
+        $data = [
+            'name' => 'New Category',
+            'user_id' => $user->id
+        ];
+
+        //Auth::login($user);
+        $this->actingAs($user);
+
+        //dd($user);
+
+        $category = $this->categoryService->addCategory($data);
+
+        //Category::factory()->create(['title' => 'New Category 1', 'user_id' => $user->id]);
+        //$this->actingAs($user);
+
+        //$response = $this->actingAs($user, 'web')->withSession(['banned' => false]);
+
+        // Call the addCategory method
+
+
+        // Assert that the returned category is an object and has the correct attributes
+        $this->assertInstanceOf(Category::class, $category);
+        $this->assertEquals('New Category', $category->name);
     }
 }
