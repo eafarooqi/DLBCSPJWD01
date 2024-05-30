d3cca757e6746ea4b0f81d9c1c7d93e3135ad703
DEEHFAR1 <ehsanahmed.farooqi@de.toyota-industries.eu>
2024-05-30 10:51:01 +0200

    update: Category Service test class

--- tests/Unit/CategoryServiceTest.php
+++ tests/Unit/CategoryServiceTest.php
@@ -6,6 +6,7 @@ use App\Models\Category;
 use App\Models\User;
 use App\Services\CategoryService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 
@@ -23,19 +24,103 @@ class CategoryServiceTest extends TestCase
         $this->categoryService = new CategoryService;
     }
 
-    public function test_add_category()
+    private function createUserForTesting(): User
     {
         $user = User::factory()->create();
-        // Sample data for the new category
+        $this->actingAs($user);
+        return $user;
+    }
+
+    public function test_add_category()
+    {
+        $user = $this->createUserForTesting();
+
+        // Data for the new category
         $data = [
             'name' => 'New Category',
         ];
 
-        $this->actingAs($user);
+
         $category = $this->categoryService->addCategory($data);
 
         // Assert that the returned category is an object and has the correct attributes
         $this->assertInstanceOf(Category::class, $category);
         $this->assertEquals('New Category', $category->name);
+        $this->assertEquals($user->id, $category->user_id);
+    }
+
+    public function test_update_category()
+    {
+        $this->createUserForTesting();
+
+        // Create a category
+        $category = Category::factory()->create([
+            'name' => 'Old Category',
+        ]);
+
+        // New data to update the category
+        $data = [
+            'name' => 'Updated Category',
+        ];
+
+        // Call the updateCategory method
+        $result = $this->categoryService->updateCategory($category, $data);
+
+        // Refresh the category instance to get the updated values
+        $category->refresh();
+
+        // Assert that the update was successful
+        $this->assertTrue($result);
+        $this->assertEquals('Updated Category', $category->name);
+    }
+
+    public function test_delete_category()
+    {
+        // creating user for testing
+        $this->createUserForTesting();
+
+        // Create a category
+        $category = Category::factory()->create([
+            'name' => 'Test Category',
+        ]);
+
+        // Call the deleteCategory method
+        $result = $this->categoryService->deleteCategory($category);
+
+        // Assert that delete result was successful
+        $this->assertTrue($result);
+
+        // Assert that the category was deleted (soft delete)
+        $this->assertSoftDeleted('categories', ['id' => $category->id]);
+    }
+
+    public function test_get_parent_categories()
+    {
+        // creating user for testing
+        $this->createUserForTesting();
+
+        // Create some parent categories
+        $parent1 = Category::factory()->create(['name' => 'Parent 1']);
+        $parent2 = Category::factory()->create(['name' => 'Parent 2']);
+
+        // Create some child categories
+        Category::factory()->create(['name' => 'Child 1', 'parent_id' => $parent1->id]);
+        Category::factory()->create(['name' => 'Child 2', 'parent_id' => $parent2->id]);
+
+        // Call the getParentCategories method
+        $result = $this->categoryService->getParentCategories();
+
+        // Assert that the result is a collection
+        $this->assertInstanceOf(Collection::class, $result);
+
+        // There should be only 2 results
+        $this->assertCount(2, $result->toArray());
+
+        // Assert that the result contains the correct parent categories
+        // as we are using ids as array index.
+        $this->assertEquals([
+            $parent1->id => 'Parent 1',
+            $parent2->id => 'Parent 2',
+        ], $result->toArray());
     }
 }
